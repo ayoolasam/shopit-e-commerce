f8fecbdb419c30e8da1cd74790c5d471e39c53d4
ayoolasam <obayomisamuel941@gmail.com>
2024-05-17 22:22:02 +0100

    commit

--- backend/Controllers/orderControllers.js
+++ backend/Controllers/orderControllers.js
@@ -1,4 +1,5 @@
 const Order = require('../Models/order.js')
+const Product = require('../Models/product.js')
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors.js')
 const ErrorHandler = require('../utils/errorHandler.js')
 
@@ -115,3 +116,92 @@ exports.getOrder = catchAsyncErrors(async(req,res,next)=>{
   }
   
   })
+  //admin get all orders /api/v1/orders/admin/orders
+  exports.allorders = catchAsyncErrors(async(req,res,next)=>{
+    try{
+    
+            const orders  = await Order.find()
+    
+    if(!orders){
+      return res.status(400).json({
+        message:"order not found"
+      })
+    }
+    
+            res.status(200).json({
+              orders
+            })
+    
+    
+    
+    
+    
+    
+    }catch(error){
+      console.log(error)
+      return res.status(404).json({
+      message:{
+          error
+      }
+    })
+    }
+    
+    })
+    
+    
+     //admin update orders /api/v1/orders/admin/:id
+  exports.updateOrder = catchAsyncErrors(async(req,res,next)=>{
+    try{
+    
+            const order  = await Order.findById(req.params.id,)
+    
+    if(!order){
+      return res.status(400).json({
+        message:"order not found"
+      })
+    }
+
+    if(order?.orderStatus === 'Delivered'){
+      return res.status(400).json({
+        message:"order has been delivered already"
+      })
+    }
+
+    //update product stock
+order?.orderItems?.forEach(async(item)=>{
+  const product = await Product.findById(item?.product?.toString())
+  product.stock = product.stock - item.quantity
+  if(!product){
+    return res.status(400).json({
+      message:"product not found with this id"
+    })
+  }
+
+  await product.save({validateBeforeSave:false})
+})
+    
+order.orderStatus = req.body.status
+order.deliveredAt = Date.now()
+
+await order.save();
+            res.status(200).json({
+              success:true
+            })
+    
+    
+    
+    
+    
+    
+    }catch(error){
+      console.log(error)
+      return res.status(404).json({
+      message:{
+          error
+      }
+    })
+    }
+    
+    })
+    
+    
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {newOrder,getOrder,myOrders} = require('../Controllers/orderControllers.js')
+const {newOrder,getOrder,myOrders,updateOrder,allorders} = require('../Controllers/orderControllers.js')
 const router = express.Router();
 const protectRoute = require('../middlewares/auth.js')
 
@@ -7,5 +7,7 @@ const protectRoute = require('../middlewares/auth.js')
 router.post('/new',protectRoute.isAuthenticatedUser,newOrder)
 router.get('/order/:id',protectRoute.isAuthenticatedUser,getOrder)
 router.get('/me/order',protectRoute.isAuthenticatedUser,myOrders)
+router.put('/admin/0rders/:id',protectRoute.isAuthenticatedUser,protectRoute.authorizeRoles('admin'),updateOrder)
+router.get('/admin/orders',protectRoute.isAuthenticatedUser,protectRoute.authorizeRoles('admin'),allorders)
 
 module.exports = router
