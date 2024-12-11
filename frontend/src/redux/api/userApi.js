6b3d0398e92df662e660251536cc85947a8069e9
ayoolasam <obayomisamuel941@gmail.com>
2024-12-11 23:58:50 +0100

    fixes

--- frontend/src/redux/api/userApi.js
+++ frontend/src/redux/api/userApi.js
@@ -19,7 +19,7 @@ export const userApi = createApi({
       //transform the result
       transformResponse: (result) => result.user,
       async onQueryStarted(args, { dispatch, queryFulfilled }) {
-        try {
+        try {cd
           //extract data from the query after it was fulfilled
           const { data } = await queryFulfilled;
           dispatch(setUser(data));
